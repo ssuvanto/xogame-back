bc360a60036e21985571f88358a543b08160fe10
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-18 12:53:02 +0300

    more advanced login

--- api/login.js
+++ api/login.js
@@ -1,9 +1,39 @@
 const token = 'xogameauthorizationtoken'
+//simple fixed array of users for now
+const users = [{
+    id: 0,
+    uname: 'Tester',
+    pword: 'testpw',
+    token: 'user0token'
+},{
+    id: 1,
+    uname: 'Usertest',
+    pword: 'pwtesting',
+    token: 'user1token'
+},{
+    id: 2,
+    uname: 'Retset',
+    pword: 'wptset',
+    token: 'user2token'
+}]
 
 export const login = (req, res) => {
     //test with single user for now and without any authentication
-    console.log(req.body.uname,'logging in')
-    res.send({token: token})
+    let token = null
+    for(let u of users){
+        if(u.uname === req.body.uname && u.pword === req.body.pword){
+            token = u.token
+        }
+    }
+    if(token){
+        //matched, send token
+        res.status(200)
+        res.send({token: token})
+    } else {
+        //no matches, send error
+        res.status(401)
+        res.send({error: 'Incorrect username or password'})
+    }
     res.end()
 }
 
@@ -13,7 +43,7 @@ export const check = (req, res, next) => {
         console.log('Success, next')
         next()
     } else {
-        console.log('Fail, 401')
-        res.sendStatus(401)
+        console.log('Fail, 403')
+        res.sendStatus(403)
     }
 }
